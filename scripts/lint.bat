b57b86f53de9560ad7bd68bbbec063aca0d8009f
Mateusz Lesiak <arkadiuszlyzka@gmail.com>
2026-06-20 10:34:15 +0200

    Lint fixed

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -1,3 +1,3 @@
 @echo off
 call .venv\Scripts\activate.bat
-python -m pylint main.py  tests/test_calculate.py tests/test_errors.py
+python -m pylint main.py tests/test_calculate.py tests/test_errors.py --exit-zero
